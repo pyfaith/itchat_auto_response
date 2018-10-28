e539604a238cf2c55e572c0bef5f481f460f4d83
pyfaith <pyfaith@foxmail.com>
2018-10-28 19:56:52 +0800

    设置缓存文件保存位置（linux系统tmp）

--- main_itchat.py
+++ main_itchat.py
@@ -5,8 +5,10 @@
 # date: 2018/10/17
 __author__ = "pyfaith"
 
+import os
 import random
 import platform
+
 
 import itchat
 import requests
@@ -15,11 +17,21 @@ KEY = ['8edce3ce905a4c1dbb965e6b35c3834d', 'eb720a8970964f3f855d863d24406576', '
 #所有的公司微信的昵称
 COMPANY_WX_NICKNAME = ["股民社区", "招财大牛猫", "随风飘逝"]
 
+#缓存文件保存位置
+_TEMP_PATH = "{sep}tmp{sep}weitchat{sep}".format(sep=os.sep)
+
+def get_temp_path():
+    if not os.path.exists(_TEMP_PATH):
+        os.mkdir(_TEMP_PATH)
+    return _TEMP_PATH
+
 def is_company_wx(msg):
     '''判断是不是公司微信'''
-    print(msg)
+    # print(msg)
     if hasattr(msg, "User"):
-        nickname = msg["User"]['NickName']
+        nickname = msg["User"]['NickName'] \
+            if hasattr(msg["User"],"NickName") else None
+
         if nickname in COMPANY_WX_NICKNAME:
             return True
     return False
@@ -41,7 +53,7 @@ def get_response(msg):
     try:
         r = requests.post(apiUrl, data=data).json()
         # 字典的get方法在字典没有'text'值的时候会返回None而不会抛出异常
-        print(r.get('text'))
+        # print(r.get('text'))
         return r.get('text')
     # 为了防止服务器没有正常响应导致程序异常退出，这里用try-except捕获了异常
     # 如果服务器没能正常交互（返回非json或无法连接），那么就会进入下面的return
@@ -72,10 +84,14 @@ def tuling_reply(msg):
 def download_files(msg):
     if is_company_wx(msg):
         #msg.download(msg['FileName'])   #这个同样是下载文件的方式
-        msg['Text'](msg['FileName'])      #下载文件
+        msg['Text'](get_temp_path() +  msg['FileName'])      #下载文件
         #将下载的文件发送给发送者
-        itchat.send('@%s@%s' % ('img' if msg['Type'] == 'Picture' else 'fil', msg["FileName"]),
-                    msg["FromUserName"])
+        itchat.send('@%s@%s' % (
+                                'img' if msg['Type'] == 'Picture' else 'fil',
+                                get_temp_path() + msg["FileName"]
+                                ),
+                    msg["FromUserName"]
+                    )
 
 
 
